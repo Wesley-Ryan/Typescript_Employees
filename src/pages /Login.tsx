df5ea6a7f07b4063274c1386cabe885d32d8345d
Wesley-Ryan <im.wesleyryan@gmail.com>
2021-07-11 14:21:39 -0700

    logo page with inputs

--- src/pages /Login.tsx
+++ src/pages /Login.tsx
@@ -1,6 +1,9 @@
-import Paper from "@material-ui/core/Paper";
+import { Paper, TextField, InputAdornment } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import Logo from "../assets/Logo.jpeg";
-const styles = {
+import { AccountCircle, Lock } from "@material-ui/icons/";
+
+const useStyles = makeStyles({
   formStyle: {
     width: "80%",
     maxWidth: "350px",
@@ -8,20 +11,46 @@ const styles = {
     margin: " 0 auto",
     marginTop: "100px",
     display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   imgStyle: {
     margin: "0 auto",
   },
-};
+});
 const Login: React.FC = () => {
+  const classes = useStyles();
   return (
     <div>
-      <Paper elevation={3} style={styles.formStyle}>
+      <Paper elevation={3} className={classes.formStyle}>
         <img
           src={Logo}
           alt="company-logo"
           height="180px"
-          style={styles.imgStyle}
+          className={classes.imgStyle}
+        />
+        <TextField
+          id="input-with-icon-textfield"
+          label="Username"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <AccountCircle />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <TextField
+          id="input-with-icon-textfield"
+          label="Password"
+          type="password"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Lock />
+              </InputAdornment>
+            ),
+          }}
         />
       </Paper>
     </div>
