0d43505915bc878be70e365f4a4f9e321f613b33
Wesley-Ryan <im.wesleyryan@gmail.com>
2021-07-12 19:51:16 -0700

    added radio buttons, grabbing newUser from formData ready to post

--- src/pages /Register.tsx
+++ src/pages /Register.tsx
@@ -1,21 +1,29 @@
-import { Paper, TextField, Button } from "@material-ui/core";
+import {
+  Paper,
+  TextField,
+  Button,
+  RadioGroup,
+  Radio,
+  FormControlLabel,
+} from "@material-ui/core";
 import { useForm, Controller } from "react-hook-form";
 import { useHistory } from "react-router-dom";
-
+import axios from "axios";
 import { makeStyles } from "@material-ui/core/styles";
 import Logo from "../assets/Logo.jpeg";
 import { Input } from "../components/Input";
+import { green } from "@material-ui/core/colors";
 
 const useStyles = makeStyles({
   formStyle: {
     width: "90%",
-    maxWidth: "350px",
-    height: "460px",
+    maxWidth: "360px",
     margin: " 0 auto",
     marginTop: "100px",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
+    padding: "15px 0 15px 0",
   },
   imgStyle: {
     margin: "0 auto",
@@ -39,12 +47,22 @@ const useStyles = makeStyles({
   btnStyle: {
     width: "140px",
   },
+  radio: {
+    display: "flex",
+    justifyContent: "space-between",
+    margin: "0 20px 0 20px",
+  },
+  radioColor: {
+    "&$checked": {
+      color: "#548d6c",
+    },
+  },
+  checked: {},
 });
 
 const Register: React.FC = () => {
   const history = useHistory();
   const { handleSubmit, control, reset } = useForm();
-
   const classes = useStyles();
 
   const onSubmit = (data: {
@@ -52,13 +70,34 @@ const Register: React.FC = () => {
     last_name: string;
     email: string;
     password: string;
+    department: string;
   }): void => {
-    console.log(data);
+    const dept = data.department.split(" ");
+    const newUser = {
+      avatar:
+        "https://ik.imagekit.io/WesleyRyan/social_preview/avatar_8kCbbtDiP.jpeg",
+      first_name: data.first_name,
+      last_name: data.last_name,
+      email: data.email,
+      password: data.password,
+      login_attempts: 0,
+      title: "",
+      salary: "",
+      role: 3893,
+      role_name: "Team Member",
+      department_name: dept[1],
+      department: parseInt(dept[0]),
+      pinpoint: "",
+      active: true,
+    };
+    console.log(newUser);
+
     reset({
       first_name: "",
       last_name: "",
       email: "",
       password: "",
+      department: "",
     });
   };
 
@@ -124,6 +163,79 @@ const Register: React.FC = () => {
               classStyle={classes.inputStyle}
             />
           </div>
+
+          <Controller
+            rules={{ required: true }}
+            control={control}
+            defaultValue="200"
+            name="department"
+            render={({
+              field: { onBlur, onChange, value },
+              fieldState: { error },
+            }) => (
+              <RadioGroup
+                value={value}
+                onBlur={onBlur}
+                onChange={(e) => {
+                  onChange(e);
+                }}
+              >
+                <div className={classes.radio}>
+                  <FormControlLabel
+                    value="300 Sales"
+                    control={
+                      <Radio
+                        classes={{
+                          root: classes.radioColor,
+                          checked: classes.checked,
+                        }}
+                      />
+                    }
+                    label="Sales"
+                  />
+                  <FormControlLabel
+                    value="200 Operations"
+                    control={
+                      <Radio
+                        classes={{
+                          root: classes.radioColor,
+                          checked: classes.checked,
+                        }}
+                      />
+                    }
+                    label="Operations"
+                  />
+                </div>
+                <div className={classes.radio}>
+                  <FormControlLabel
+                    value="400 IT"
+                    control={
+                      <Radio
+                        classes={{
+                          root: classes.radioColor,
+                          checked: classes.checked,
+                        }}
+                      />
+                    }
+                    label="IT"
+                  />
+                  <FormControlLabel
+                    value="100 Engineering"
+                    control={
+                      <Radio
+                        classes={{
+                          root: classes.radioColor,
+                          checked: classes.checked,
+                        }}
+                      />
+                    }
+                    label="Engineering"
+                  />
+                </div>
+              </RadioGroup>
+            )}
+          />
+
           <div className={classes.BtnGroup}>
             <Button
               className={classes.btnStyle}
